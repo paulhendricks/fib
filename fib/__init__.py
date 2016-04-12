6594c7d68c20f991e5442d659267fc58e0e8cacb
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-04-12 11:38:03 -0400

    Template for package

---
+++ fib/__init__.py
@@ -0,0 +1,8 @@
+# fib/__init__.py
+
+__version__ = '0.1.0'
+
+from .fib import *
+
+__all__ = []
+__all__ += fib.__all__
---
+++ setup.py
@@ -0,0 +1,14 @@
+__author__ = 'Paul Hendricks'
+from setuptools import setup
+
+setup(name='fib',
+      version='0.1.0',
+      description='description',
+      url='url',
+      author='Paul Hendricks',
+      author_email='paul.hendricks.2013@owu.edu',
+      license='license',
+      packages=['fib'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'])
