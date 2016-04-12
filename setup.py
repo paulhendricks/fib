849c7779a8253c4435e9bb46437263375c7b03b6
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-04-12 12:20:22 -0400

    Working!

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
-__author__ = 'Paul Hendricks'
-from setuptools import setup
+from distutils.core import setup
+from Cython.Build import cythonize
 
-setup(name='fib',
-      version='0.1.0',
-      description='description',
-      url='url',
-      author='Paul Hendricks',
-      author_email='paul.hendricks.2013@owu.edu',
-      license='license',
-      packages=['fib'],
-      zip_safe=False,
-      test_suite='nose.collector',
-      tests_require=['nose'])
+
+setup(ext_modules=cythonize('fib.pyx'))
