74c409c95da85a34ed14e3c0a7ffabae1cd8551c
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-04-14 11:40:23 -0400

    added documentation

--- fib/pyfib.py
+++ fib/pyfib.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+"""Module for Cython code.
+
+Description.
+
+Available defs:
+- cyfibf: Returns the nth Fibonacci number.
+"""
+
 from .cyfib import cyfibf
 
 
@@ -5,13 +14,16 @@ __all__ = ['pyfibf', 'hello_world', 'wrapper']
 
 
 def pyfibf():
+    """Returns the nth Fibonacci number."""
     print("Hello world! This is pyfib()")
 
 
 def hello_world():
+    """Returns the nth Fibonacci number."""
     print("Hello world!")
 
 
 def wrapper(n):
+    """Returns the nth Fibonacci number."""
     print("This is a wrapper")
     return cyfibf(n)
