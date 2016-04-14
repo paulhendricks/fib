a81b4a8bd9446d31281d630ea38c66eb07cb4b35
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-04-14 11:25:51 -0400

    updates

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ git clone https://github.com/paulhendricks/fib
 cd fib
 python3 setup.py build_ext --inplace
 ```
+
+To run, please execute the following in any Python kernel:
+```Python
+import fib
+fib.pyfib.wrapper(100)
+```
--- fib/cyfib.pyx
+++ fib/cyfib.pyx
@@ -1,5 +1,15 @@
+#!/usr/bin/python3
+"""Module for Cython code.
+
+Description.
+
+Available defs:
+- cyfibf: Returns the nth Fibonacci number.
+"""
+
+
 def cyfibf(long n):
-    '''Returns the nth Fibonacci number.'''
+    """Returns the nth Fibonacci number."""
     cdef long a=0, b=1, i
     for i in range(n):
         a, b = a + b, a
